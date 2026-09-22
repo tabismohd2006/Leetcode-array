93df58931dbbe0e91605514327228bc0194a5ccd
mohd tabis <tabismohd676@gmail.com>
2026-09-23 00:13:00 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (32.54%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,19 @@
+class Solution{
+public static int lengthOfLastWord(String s) {
+
+    int i = s.length() - 1;
+
+    while (i >= 0 && s.charAt(i) == ' ') {
+        i--;
+    }
+
+    int count = 0;
+
+    while (i >= 0 && s.charAt(i) != ' ') {
+        count++;
+        i--;
+    }
+
+    return count;
+}
+}
